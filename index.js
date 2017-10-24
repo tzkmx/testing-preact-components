ea2bb4babf18342d61d9bb0ed30120d88ae22c64
Jesus E. Franco Martinez <jefrancomix@gmail.com>
2017-10-24 12:18:35 -0500

    Simple interaction to test preact-render-spy

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 import './style';
 import { Component } from 'preact';
+import Dimlight from './components/Dimlight';
 
 export default class App extends Component {
-	render() {
+
+	componentDidMount() {
+		this.setState({op:0.5});
+	}
+
+	moreOpacity() {
+		let op = this.state.op;
+		this.setState({op:op + 0.1})
+	}
+	lessOpacity()  {
+		let op = this.state.op;
+		this.setState({op:op - 0.1})
+	}
+
+	render(props, {op}) {
 		return (
 			<div>
-				<h1>Hello, World!</h1>
+				<h1 onClick={this.moreOpacity.bind(this)}>Hello, World!</h1>
+				<Dimlight op={op}/>
+				<p onClick={this.lessOpacity.bind(this)}>Test</p>
 			</div>
 		);
 	}
