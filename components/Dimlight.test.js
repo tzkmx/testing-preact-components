e0f83bc1c3835d33e467ad677e9f8f1cf8ed473d
Jesus E. Franco Martinez <jefrancomix@gmail.com>
2017-10-26 18:42:01 -0500

    Not compiling tests: undefined in rendered to string

---
+++ components/Dimlight.test.js
@@ -0,0 +1,18 @@
+import {h} from 'preact';
+import {deep} from 'preact-render-spy';
+import {render} from 'preact-render-to-string';
+import {Dimlight} from './Dimlight';
+
+it('lets you do cool things with preact components', () => {
+    let comp = <Dimlight op="0.3"></Dimlight>;
+    const context = deep(comp);
+    console.log(render(comp));
+    console.log(context.find('p'));
+    expect(context.find('p').contains("Yeah baby")).toBeTruthy();
+    /*
+    context.find('[onClick]').simulate('click');
+
+    expect(context.find('p').attr('style')).toBe({opacity:0.6});
+     */
+});
+
